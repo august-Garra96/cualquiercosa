9a535a3604c5ac98b976903167fcb488a2456214
Dennis Augusto Garrafa Solis <za16011264@zapopam.tecmm.edu.mx>
2020-09-21 17:52:54 -0500

    Ahora es una Suma de números haha xd

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 
 
-def print_hi(name):
-
-    print(f'Hola, {name}')
-
+def suma():
+    num1 = int(input("dame un número:"))
+    num2 = int(input("dame otro número:"))
+    print("la súma es: ",num1 + num2)
 
 
 if __name__ == '__main__':
-    print_hi('Dennis Augusto Garrafa Solis')
+    suma()
+
 
