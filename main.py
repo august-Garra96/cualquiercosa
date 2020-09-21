fd3577e165c6c2f56a1c3b0153da894556aced9e
Dennis Augusto Garrafa Solis <za16011264@zapopam.tecmm.edu.mx>
2020-09-21 17:54:32 -0500

    Ajustando espacios

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 
 def suma():
-    num1 = int(input("dame un número:"))
-    num2 = int(input("dame otro número:"))
+    num1 = int(input("dame un número: "))
+    num2 = int(input("dame otro número: "))
     print("la súma es: ",num1 + num2)
 
 
